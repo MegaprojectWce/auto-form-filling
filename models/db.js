6e77d784bdf6e105b413ed2a927150d453cd95dd
manojm01 <manojmetgud035@gmail.com>
2023-03-14 14:37:16 +0530

    comment removed

--- models/db.js
+++ models/db.js
@@ -10,14 +10,6 @@ mongoose.connect('mongodb+srv://manojmetgud035:sem8Mega@cluster0.oykyxlh.mongodb
     else {console.log('Error in DB Connection: '+ err);}
 
 });
-// mongoose.connect('mongodb://localhost:27017/EmployeeDB', {useNewUrlParser:true} , (err)=>{
 
-//     if(!err){console.log('MongoDB Connection Succeeded');}
-//     else {console.log('Error in DB Connection: '+ err);}
-
-// });
 
 require('./form.models');
-
-
-// mongodb+srv://manojmetgud035:<password>@cluster0.oykyxlh.mongodb.net/?retryWrites=true&w=majority
