0469116debeb7bb97f68b3007942380008ea54a9
Ashokav008 <ashokwavre.wce@gmail.com>
2023-05-26 00:30:13 +0530

    input border

--- models/db.js
+++ models/db.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 // console.log(process.env.USER_NAME);
 // console.log(process.env.PASS_WORD);
 
-mongoose.connect('mongodb+srv://manojmetgud035:sem8Mega@cluster0.oykyxlh.mongodb.net/dataDB', {useNewUrlParser:true} , (err)=>{
+mongoose.connect(
+  "mongodb+srv://manojmetgud035:sem8Mega@cluster0.oykyxlh.mongodb.net/dataDB",
+  { useNewUrlParser: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded");
+    } else {
+      console.log("Error in DB Connection: " + err);
+    }
+  }
+);
 
-    if(!err){console.log('MongoDB Connection Succeeded');}
-    else {console.log('Error in DB Connection: '+ err);}
-
-});
-
-
-require('./form.models');
+require("./form.models");
